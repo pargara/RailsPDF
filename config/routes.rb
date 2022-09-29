f244a0569ae6288dd102a51567b6c6bad71859c9
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2022-09-29 16:08:12 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :posts
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
